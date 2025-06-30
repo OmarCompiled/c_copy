4baf73709aaf1f32e13d158b47e6777f31e0147c
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-06-30 21:23:02 +0300

    Minor style fix

--- src/type_check.c
+++ src/type_check.c
@@ -6,8 +6,8 @@ short int
 is_dir(const char* dest_name) {
   DIR* dir;
   if((dir = opendir(dest_name)) != NULL) {
-		closedir(dir);
-    return 1;
+			closedir(dir);
+			return 1;
 	}
 
   return 0;
@@ -17,8 +17,8 @@ short int
 is_existing_file(const char* file_name) {
   FILE* file;
   if((file = fopen(file_name, O_RDONLY)) != NULL){
-		fclose(file);
-    return 1;
+			fclose(file);
+    	return 1;
 	}
 
   return 0;
