309dc41afa93f5139e65d5b9bb618c1ffbe04bb8
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-07-04 12:36:02 +0300

    Change hardcoded buffer for read and Use while loop instead to read any file
    Finallyyyyyy
    Alhamdu lillah

--- src/copy.c
+++ src/copy.c
@@ -8,47 +8,43 @@
 
 #include "type_check.h"
 
-/* I understand 4096 bytes isn't always enough,
-     this is just an arbitrary number. I'll try
-     to fix it later.
-*/
-#define BUFFER_SIZE 4096 /*4 Kib*/
-
 int copy(const char *src, const char *dest_name) {
-  char buffer[BUFFER_SIZE];
-  int bytes_read;
+	int bytes_read;
   int source;
   int dest;
+	char buffer;
 
   /* Since most -if not all- return -1 on failure,
      that's what I'll test for. Perhaps I'll change this to errno later on.*/
-  if ((source = open(src, O_RDONLY)) < 0) {
+  if ((source = open(src, O_RDONLY | O_APPEND)) < 0) {
     fprintf(stderr, "error copying file %s: %s\n", src, strerror(errno));
 
     return -1;
   }
 
   if ((dest = open(dest_name, O_WRONLY | O_CREAT, 0644)) < 0) {
-    perror(NULL);
+    perror("open");
 
     return -1;
   }
 
-  if ((bytes_read = read(source, buffer, sizeof(buffer))) < 0) {
-    fprintf(stderr, "error reading file %s: %s\n", src, strerror(errno));
+  while((bytes_read = read(source, &buffer, 1)) && bytes_read != 0) {
+		if(bytes_read < 0) {
+			perror("read");
 
-    return -1;
-  }
+			return -1;
+		}
 
-  if (write(dest, buffer, bytes_read) < 0) {
-    perror(NULL);
+		if(write(dest, &buffer, 1) < 0) {
+			perror("write");
 
-    return -1;
-  }
+			return -1;
+		}
+	}
 
   if (close(source) < 0 || close(dest) < 0) {
     /* Not sure what to print */
-    perror(NULL);
+    perror("close");
 
     return -1;
   }
@@ -66,7 +62,8 @@ int copy_multiple_files(const int argc, char **argv, const char *dest_name) {
     /* I have to initialize space since malloc
              just allocates and doesn't initialize */
     *updated_dest_name = '\0';
-
+		
+		/* Realized I should fix copy, not this function :) */
     if (is_dir(argv[i]) && is_dir(dest_name)) {
       continue;
     }
@@ -81,6 +78,8 @@ int copy_multiple_files(const int argc, char **argv, const char *dest_name) {
 
     if (copy(argv[i], updated_dest_name) < 0) {
       /* error messages from copy() are enough, just returning */
+			free(updated_dest_name); /* to prevent memory leak :) */
+
       return -1;
     }
 
